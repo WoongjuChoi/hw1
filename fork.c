3af055d2625235421c687418e4566c3d02bcc7a1
Seehwan Yoo <seehwan.yoo@assam.dankook.ac.kr>
2019-09-25 11:14:44 +0900

    fork example with multiple child
    	sending signal to child procs.
    
    	P: terminates after 30 ticks
    	C: terminates after 3 ticks execution

--- fork.c
+++ fork.c
@@ -3,24 +3,27 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	pid_t pid;
+	int i;
 
-	pid = fork();
-	if (pid == -1) {
-		perror("fork error");
-		return 0;
-	}
-	else if (pid == 0) {
-		// child
-		printf("child process with pid %d\n", 
-			getpid());
-	} else {
-		// parent
-		printf("my pid is %d\n", getpid());
-		wait(0);
+	for (i = 0 ; i < 10 ; i++) {
+		pid = fork();
+		if (pid == -1) {
+			perror("fork error");
+			return 0;
+		}
+		else if (pid == 0) {
+			// child
+			printf("child process with pid %d (i: %d) \n", getpid(), i);
+			exit (0);
+		} else {
+			// parent
+			wait(0);
+		}
 	}
 	return 0;
 }
