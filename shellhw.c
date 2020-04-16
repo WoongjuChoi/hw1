33eb78f9e93e7bdc71c32c6f80a0df82fb347864
seungyeon Shin <ssy971216@naver.com>
2020-04-16 17:37:24 +0900

    commit

--- shellhw.c
+++ shellhw.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+int main(){
+        while(1){
+		pid_t pid;
+		pid=fork();
+		if(pid==-1){
+			perror("fork error");
+			return 0;
+		}
+		else if(pid == 0){
+			char *argv[2];
+			argv[0]="/bin/sh";
+			argv[1]=NULL;
+                        execve(argv[0],argv,NULL);
+			printf("d\n");
+			exit(0);
+		}
+		else {
+			wait(0);
+		}
+	}
+	return 0;
+}
