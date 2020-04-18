d5316a9841e364f35cee5434fbe4217e0f8520f2
seungyeon Shin <ssy971216@naver.com>
2020-04-18 10:37:13 +0900

    test

--- shellhw.c
+++ shellhw.c
@@ -1,27 +1,46 @@
 #include<stdio.h>
+#include<stdlib.h> 
 #include<unistd.h>
-#include<stdlib.h>
-#include<sys/wait.h>
+#include<string.h>
 #include<sys/types.h>
-int main(){
-        while(1){
-		pid_t pid;
-		pid=fork();
-		if(pid==-1){
-			perror("fork error");
-			return 0;
-		}
-		else if(pid == 0){
-			char *argv[2];
-			argv[0]="/bin/sh";
-			argv[1]=NULL;
-                        execve(argv[0],argv,NULL);
-			printf("d\n");
-			exit(0);
-		}
-		else {
-			wait(0);
-		}
-	}
-	return 0;
+#include<sys/wait.h>
+#define MAX_PATH 100
+#define LF 10
+int print_prompt(){
+	char *buffer =(char *)malloc(MAX_PATH);
+	char *value=(char*)getcwd(buffer,MAX_PATH);
+       	if(value !=0)	fprintf(stdout,"shell:%s>",buffer);
+	free(buffer);
+        return 0;
 }
+int main(int argc,char *argv[],char *envp[]){
+       char c;
+       char cmd[80];
+       char *vecl[10];
+       setbuf(stdout,NULL);
+       print_prompt();
+       while(fgets(cmd,80,stdin)!=NULL){
+	       int i=0;
+	       cmd[strlen(cmd)-1]='\0';
+	       memset(vecl,0x00,sizeof(vecl));
+	       char *token;
+	       token=strtok(cmd,"");
+	       while(token){
+		    vecl[i]=token;
+		    token=strtok(NULL,"");
+		    i++;
+		   }
+             if(fork()==0){
+		     char path[100]="/bin/";
+		     strcat(path,vecl[0]);
+		     execve(path,vecl,envp);
+		     exit(0);
+	     }
+	     else{
+		     wait(0);
+		     print_prompt();
+		 }
+       }
+       return 0;
+}
+
