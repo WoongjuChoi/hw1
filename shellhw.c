c3fd93bd8feedc8e8f45f0c25e1b07822aec5abc
seungyeon Shin <ssy971216@naver.com>
2020-04-19 13:29:17 +0900

    shell

--- shellhw.c
+++ shellhw.c
@@ -1,15 +1,23 @@
-#include<stdio.h>
+#include<stdio.h>  
 #include<stdlib.h> 
 #include<unistd.h>
 #include<string.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<signal.h>
+#include<stdbool.h>
+#include<sys/stat.h>
 #define MAX_PATH 100
 #define LF 10
+#define TRUE 1
+#define FALSE 0
+#define ERROR (-1)
 int print_prompt(){
 	char *buffer =(char *)malloc(MAX_PATH);
 	char *value=(char*)getcwd(buffer,MAX_PATH);
-       	if(value !=0)	fprintf(stdout,"shell:%s>",buffer);
+       	if(value !=0){
+		fprintf(stdout,"shell:%s>",buffer);
+	}
 	free(buffer);
         return 0;
 }
@@ -30,9 +38,9 @@ int main(int argc,char *argv[],char *envp[]){
 		    token=strtok(NULL,"");
 		    i++;
 		  }
-	        if(!strncmp(cmd,"quit",strlen(cmd))){
-			       return -1;
-		}
+             if(!strcmp(vecl[0],"quit")){
+		     exit(1);
+		    }
              if(fork()==0){
 		     char path[100]="/bin/";
 		     strcat(path,vecl[0]);
