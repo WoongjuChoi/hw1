6ae4cc0638f47d1d110a6bbbb1ba06f60ce04274
sunghoon777 <sunghoonlee7@naver.com>
2020-04-20 03:51:31 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include<string.h>
-
+#include <string.h>
+#include <time.h>
+//프롬프트 함수
 void type_prompt()
-{
-	pritf("#");
+{ 
+    static int first_time = 0;
+    if(first_time == 0){
+	time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+	printf(" user name : %s user path : %s  present time : %d-%d-%d %d:%d:%d\n", getenv("USER"),getenv("PWD"),tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	printf("#");
+	}
+	else {
+		printf("#");
+	}
 }
-
+//사용자 명령 읽기
 void read_command(char cmd[], char* par[])
 {
 	char line[1024];
@@ -17,7 +27,7 @@ void read_command(char cmd[], char* par[])
 
 	//read line
 	for (;;) {
-		int c = fgetc(stdin);
+		int c = fgetc(stdin); 
 		line[count++] = (char)c;
 		if (c == '\n')break;
 	}
@@ -35,24 +45,33 @@ void read_command(char cmd[], char* par[])
 }
 
 
-
-
 int main(void)
 {
-	char cmd[1000], command[100], * parameters[20];
-	char* envp[] = { (char*)"PATH=/BIN",0 }; 
+	char cmd[1000], command[100], * parameters[20]; //cmd, command는 명령어들을 저장하는 변수이다 .
+	char* envp[] = { (char*)"PATH=/bin",0 };  
+    pid_t pid;
+    int i =0;  //프로세스 제어용 변수
+	type_prompt();
+	pid = fork();
+    //무한루프 시작         
 	while (1) {
-		type_prompt();
-		read_command(command, parameters);
-		if (fork() != 0)
-			wait(NULL);
-		else {
+		if (pid == -1) {
+			perror("fork error");
+			return 0;
+		}
+		if (pid == 0) {
+			type_prompt();
+		    read_command(command, parameters);
 			strcpy(cmd, "/bin/");
 			strcat(cmd, command);
 			execve(cmd, parameters, envp);
+			if (strcmp(command, "exit") == 0)
+				exit(0);
 		}
-		if (strcmp(command, "exit") == 0)
-			break;
+		else {
+			wait(0);
+			return 0;
+		}
 	}
 	return 0;
 }
