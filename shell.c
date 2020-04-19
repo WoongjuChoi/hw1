bb19e6015bdebca0866c52e1cc4e0da06b7bf5c8
leeminjung1 <vipminjung@gmail.com>
2020-04-19 17:12:44 +0900

    0419

--- shell.c
+++ shell.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
     pid_t pid;
     char *path;//현재 경로
     path=getcwd(NULL, MAX); //현재 디렉토리 가져오기
+    char*value;
 
     while(1) {
 	printf("[SimpleShell] %s : ", path);
@@ -22,15 +23,22 @@ int main(int argc, char *argv[]) {
 
 	if(buf[0]=='\n')	//enter처리
 		continue;
-
+	
         buf[strlen(buf) - 1] = '\0'; //개행문자없애기
-        if(!strncmp(buf, "quit", strlen(buf))) {
+        
+	value = getenv(buf);
+	if(value!=NULL){
+		printf("%s = %s\n", buf,value);
+	}	
+	
+	if(!strncmp(buf, "quit", strlen(buf))) {
             return -1;
         }
 	
 	if(!strncmp(buf, "pwd", strlen(buf))){
 		printf("%s\n",path);
 	}
+	
 
         pid = fork();
         if(pid < 0) {
