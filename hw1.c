2b1075e6dd976a7f9cc19dcd5fac01d168e5d129
sunghoon777 <sunghoonlee7@naver.com>
2020-04-20 20:22:57 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -7,40 +7,30 @@
 //프롬프트 함수
 void type_prompt()
 { 
-    static int first_time = 0;
-    if(first_time == 0){
-	time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-	printf(" user name : %s user path : %s  present time : %d-%d-%d %d:%d:%d\n", getenv("USER"),getenv("PWD"),tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-	printf("if you want to exit child process, press exit key\n");
-	printf("#");
-	first_time++;
-	}
-	else {
 		printf("#");
-	}
 }
 //사용자 명령 읽기
 void read_command(char cmd[], char* par[])
 {
 	char line[1024];
 	int count = 0, i = 0, j = 0;
-	char * array[100], * pch;
-
-	//read line
-	for (;;) {
-		int c = fgetc(stdin); 
+	char* array[100], * pch;
+	for (;;)
+	{
+		int c = fgetc(stdin);
 		line[count++] = (char)c;
 		if (c == '\n')break;
+
 	}
 	if (count == 1)return;
 	pch = strtok(line, "\n");
 
-	while (pch != NULL) {
+	while (pch !=NULL)
+	{
 		array[i++] = strdup(pch);
 		pch = strtok(NULL, "\n");
 	}
-	strcpy(cmd, array[0]);
+
 	for (int j = 0; j < i; j++)
 		par[j] = array[j];
 	par[i] = NULL;
@@ -58,7 +48,7 @@ int main(void)
 			wait(NULL);
 		else
 		{
-			strcpy(cmd. "/bin/");
+			strcpy(cmd "/bin/");
 			strcat(cmd, command);
 			execve(cmd, parameters, envp);
 		}
