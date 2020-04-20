54c2a1ce982ed382786a12609a59b01498cc59bf
gaYoung Kim <gyk7@naver.com>
2020-04-20 23:46:59 +0900

    hw1

--- shell.c
+++ shell.c
@@ -1,43 +1,52 @@
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-int main()
+#define MAX_LINE 1000
+
+int main(int argc, char**argv)
 {
-	int pid;
+	char buf[MAX_LINE];
 	int status;
+	int pid;
 	int i = 1;
 	pid_t pid_child;
 
-	pid = fork();
-	if( pid == -1 )
+	printf("---Ga Young Shell---");
+	while(1)
 	{
-		fprintf(stderr, "fork error");
-		exit(0);
-	}
-	else if ( pid == 0 )//자식 프로세스
-	{
-		printf("I am child!\n");
-		while(i<20)
+		fgets(buf,MAX_LINE,stdin);
+		if(strncmp(buf,"quit\n",10)==0)
 		{
-			printf("child number :%d\n", i);
-			i++;
-			sleep(1);
+			break;
 		}
-		execlp("/bin/ls", "/bin/ls", "-1",NULL);
-		return 0;
-		exit(0);
-
+		pid = fork();
+		if(pid == -1 )
+		{
+			fprintf(stderr, "fork error");
+			exit(0);
+		}
+		else if(pid == 0)//자식 프로세스
+		{
+			printf("I am child!\n");
+			while(i < 20)
+			{
+				printf("child number : %d\n", i);
+				i++;
+				sleep(1);
+			}
+			execlp("/bin/ls","/bin/ls","-1",NULL);
+			return 0;
+			exit(0);
+		}
+		else // 부모 프로세스
+		{
+			printf("I'm parent, for waiting	for Child process\n");
+			pid_child = wait(&status);
+			printf("Child pid = %d\n", pid_child);
+		}
 	}
-	else //부모 프로세스
-	{
-		printf("I'm parent, and I'm waiting for Child proces\n");
-		pid_child = wait(&status);
-		printf("Child pid = %d\n", pid_child);
-	}
-	printf("---The end---\n");
-	return 0;
-
 }
