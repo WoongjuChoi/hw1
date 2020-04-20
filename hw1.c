e0d913f497b8ce5adf7c05f3e2fd55d15041588a
GyuriShim <rabitskr@naver.com>
2020-04-20 20:08:11 +0900

    Add other functions
    add pwd, cd, file execution

--- hw1.c
+++ hw1.c
@@ -19,21 +19,16 @@ int main(int argc, char *argv[], char *envp[])
 	printf("********************hw1*******************\n");
 
 	while(1){
-		getcwd(path, BUFFER);
-		if(path ==  NULL){
-			perror("current working directory get error");
-			return 0;
-		}
 
-		printf("SiSH..%s$", path);
-		fgets(file_name, BUFFER, stdin);
-		if ((strcmp(file_name, "\n")==0))
+		printf("SiSH..%s$", argv[0]);//현재 경로 출력
+		fgets(file_name, BUFFER, stdin);//사용자에게 입력받음
+		if ((strcmp(file_name, "\n")==0))//공백 입력시 다시 입력받음
 			continue;
 
 		int i = 0;
-		char *ptr = strtok(file_name," ");
+		char *ptr = strtok(file_name," ");//입력을 공백 기준으로 구분
 		
-		while(ptr != NULL){
+		while(ptr != NULL){//공백을 기준으로  구분하여 저장
 			fArr[i] = ptr;
 			i++;
 
@@ -41,34 +36,58 @@ int main(int argc, char *argv[], char *envp[])
 		}
 
 		pid = fork();
-		if (pid == -1){
+		if (pid == -1){//부모 프로세스
 			perror("fork error");
 			return 0;
 		}
-		else if(pid == 0){
-			if (strcmp(fArr[0], "quit")==0){
+		else if(pid == 0){//자식 프로세스
+			if (strcmp(fArr[0], "quit")==0){//"quit"을 입력받았을 때 종료
 				exit(0);
 			}
-			else if(strcmp(file_name, "ls\n")==0){
+			else if(strcmp(file_name, "ls\n")==0){//"ls"를 입력받으면 ls 실행
 				execlp("/bin/ls","ls",NULL);
 				exit(0);
 			}
-			//else if(strcmp(fArr[0], "cd")==0){
-			//	int cDir = chdir(fArr[1]);
+			else if(strcmp(fArr[0], "cd")==0){ //디렉토리 변경(동작 안함)
+				char cPath[BUFFER], cdPath[BUFFER];
+				getcwd(cPath, BUFFER);
+				strcat(cdPath,"/");
+				strcat(cdPath,fArr[1]);
+				int cDir = chdir(cdPath);
 
-			//	if(cDir == -1)
-			//		perror("directory change failed\n");
-			//	exit(0);
-			//}
-			else
+				if(cDir == -1)//변경 안될때 에러 발생
+					perror("directory change failed");
 				exit(0);
-			
+			}
+			else if(strcmp(file_name, "pwd\n")==0){//pwd 구현
+				getcwd(path, BUFFER); //현재 경로
+ 			               if(path ==  NULL){ //경로의 길이가 psth의 길이보다 길면 출력 후 종료
+                        			perror("current working directory get error");
+						exit(0);
+					}
+				printf("%s\n", path);//경로 출력
+				exit(0);
+			}
+			else{
+				char *fPath = strtok(argv[0], " .:"); //파일 실행하는 부분(동작안힘)
+
+				while(fPath != NULL){
+					fPath = strtok(NULL," .:");
+				}
+				strcat(fPath, fArr[0]);
+				
+				char *argv[] = {fArr[0]};
+
+				execve(fPath, argv, NULL);
+				
+				exit(0);
+			}
 		}
 		else{
-			wait(0);;
+			wait(0);
 		}
 		
-		if(strcmp(file_name, "quit\n")==0)
+		if(strcmp(file_name, "quit\n")==0)//반복문 종료
 			break;
 	}
 	printf("\nhw1.c end\n");
