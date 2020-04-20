3e6c9af35ff179808f1f4ad221b757b73b1e5d8a
leeminjung1 <vipminjung@gmail.com>
2020-04-20 16:52:00 +0900

    Simple Shell
    FINAL

--- shell.c
+++ shell.c
@@ -4,39 +4,28 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-const int MAX=1024;
-
-int main(/*int argc, char *argv[]*/) {
-
-    char buf[256];
+#define MAX 256
+void tockenize(char*);
+void cd(char*);
+int argv;
+char *ret_ptr;
+char *next_ptr;
+char *arg[MAX];
+int main() {
+    char buf[MAX];
     char*value;
-    char *path;//현재 경로
-    path=getcwd(NULL, MAX); //현재 디렉토리 가져오기
-    char *ret_ptr;
-    char *next_ptr;
-    int argv;
-    char *arg[MAX];
-    static const char delim[] = " \t\n"; //공백,탭,개행으로 구분
     pid_t pid;
 
     while(1) {
 
-	printf("[SimpleShell] %s : ", path);
+	printf("[SimpleShell] %s : ", getcwd(NULL,MAX));
 	memset(buf, 0, sizeof(buf));
         fgets(buf, sizeof(buf) - 1, stdin);
-	argv=0;
-	
-       	ret_ptr = strtok_r(buf, delim, &next_ptr);
-
-	while(ret_ptr){
-		arg[argv++]=ret_ptr;
-		ret_ptr=strtok_r(NULL,delim,&next_ptr);
-	}
-	arg[argv] = (char*)0;
 
 	if(buf[0]=='\n')	//enter처리
 		continue;
-	
+
+	tockenize(buf);
 
 	//PATH, USER, time, ...
 	value = getenv(buf);
@@ -49,37 +38,45 @@ int main(/*int argc, char *argv[]*/) {
             return -1;
         }
 	
-
-	*/
 	//cd
-	if(!strcmp(arg[0],"cd")){
-		if(arg[1]!=NULL){
-			path = arg[1];	//인자가 있는경우
-		}
-		else if((path = (char*)getenv("HOME")) ==NULL){
-			path = getcwd(NULL,MAX);	//인자가 없는 경우 HOME
-		}
-		path=getcwd(path,MAX);
-		if(chdir(path) == -1){
-			fprintf(stderr,"%s: wrong directory\n",path);
-		}
+	else if(!strcmp(arg[0],"cd")){
+		cd(arg[1]);
 	}
-	*/
 	
 	//fork
 	if ((pid=fork()) == -1) {
 		perror("fork error");
 		return 0;
 	}
-	else if (pid != 0) {
-		// Parent: waiting for child quit
+	else if (pid != 0) {	// Parent: waiting for child quit
 		wait(0);
-	} else {
-		// Child
+	} else {		// Child
 		execvp(arg[0],arg);
                 exit (0);
 	}
     }
 
     return 0;
+}
+
+void tockenize(char buf[MAX]){
+	static const char delim[] = " \t\n";
+	argv = 0;
+	ret_ptr = strtok_r(buf, delim, &next_ptr);
+	while(ret_ptr) {
+		arg[argv++] = ret_ptr;
+		ret_ptr = strtok_r(NULL, delim, &next_ptr);
+	}
+	arg[argv] = (char*)0;
+}
+
+void cd(char* target){
+	char name[MAX];
+	if(chdir(target) == -1) {
+		chdir(getenv("HOME"));
+	}
+	getcwd(name,MAX);
+	printf("%s\n",name);
+	if(setenv("PWD",name,1)==-1)
+		printf("fail");
 }
